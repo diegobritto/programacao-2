08c6ddbc6f8759cb8da7440975228a91aeaeb2d5
Diego Britto <diego.britto@safeweb.com.br>
2020-11-10 11:11:09 -0300

    Criação do menu

--- lab4/src/com/company/Main.java
+++ lab4/src/com/company/Main.java
@@ -4,6 +4,7 @@ import entidade.Paciente;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
 
@@ -12,19 +13,59 @@ public class Main {
 
     public static void main(String[] args) {
         init();
-
+        menu();
     }
 
     public static void init() {
         pacientesNãoVacinados.add(0, new Paciente("Joana", 25, false));
-        pacientesNãoVacinados.add(1, new Paciente("Beatriz", 60,false));
-        pacientesNãoVacinados.add(2, new Paciente("Paulo", 76,false));
-        pacientesNãoVacinados.add(3, new Paciente("Joao", 40,false));
-        pacientesNãoVacinados.add(4, new Paciente("Isabella", 9,false));
+        pacientesNãoVacinados.add(1, new Paciente("Beatriz", 60, false));
+        pacientesNãoVacinados.add(2, new Paciente("Paulo", 76, false));
+        pacientesNãoVacinados.add(3, new Paciente("Joao", 40, false));
+        pacientesNãoVacinados.add(4, new Paciente("Isabella", 9, false));
         Collections.sort(pacientesNãoVacinados);
     }
 
-    public static void menu(){
+    public static void menu() {
+        Scanner in = new Scanner(System.in);
+        int escolhaMenu=-1;
 
+        while (escolhaMenu!=0) {
+            System.out.println(":::::::::::::::Menu::::::::::::::::");
+            System.out.println(":::1- Vacinar o primeiro da fila:::");
+            System.out.println(":::2- Mostrar vacinados         :::");
+            System.out.println(":::3- Mostrar não vacinados     :::");
+            System.out.println(":::0- Sair                      :::");
+            System.out.println(":::::::::::::::::::::::::::::::::::");
+            escolhaMenu = in.nextInt();
+
+            switch (escolhaMenu) {
+                case 1:
+                    pacientesNãoVacinados.get(0).Vacinar();
+                    pacientesVacinados.add(pacientesNãoVacinados.get(0));
+                    pacientesNãoVacinados.remove(0);
+                    break;
+                case 2:
+                    System.out.println("::::::::::Paciêntes  vacinados:::::::::::::");
+                    mostrarLista(pacientesVacinados);
+                    break;
+                case 3:
+                    System.out.println("::::::::::Paciêntes não vacinados::::::::::");
+                    mostrarLista(pacientesNãoVacinados);
+                    break;
+                case 0:
+                    return;
+                default:
+
+            }
+        }
+    }
+
+    public static void mostrarLista(ArrayList<Paciente> lista) {
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println(
+                    "id: " + i + "\n" +
+                    lista.get(i).toString()
+            );
+        }
     }
 }
--- lab4/src/entidade/Paciente.java
+++ lab4/src/entidade/Paciente.java
@@ -1,52 +1,32 @@
 package entidade;
 
-public  class Paciente implements Comparable <Paciente>{
+public class Paciente implements Comparable<Paciente> {
     private String nome;
     private int idade;
     private boolean vacinado;
 
-    public Paciente(String nome, int idade, boolean vacinado){
+    public Paciente(String nome, int idade, boolean vacinado) {
         this.idade = idade;
         this.nome = nome;
         this.vacinado = vacinado;
     }
 
-    public int getIdade() {
-        return idade;
+    public void Vacinar() {
+        this.vacinado = true;
     }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setVacinado(boolean vacinado) {
-        this.vacinado = vacinado;
-    }
-
-    public boolean  getVacinado() {
-        return this.vacinado;
+    @Override
+    public int compareTo(Paciente outroPaciente) {
+        if (this.idade > outroPaciente.idade) {
+            return -1;
+        }
+        return 0;
     }
 
     @Override
     public String toString() {
         return "Nome: " + nome + "\n" +
-                "Idade: " + idade +"\n"+
-                "Vacinado=" + vacinado;
-    }
-
-    @Override
-    public int compareTo(Paciente outroPaciente) {
-        if (this.idade < outroPaciente.idade) {
-            return -1;
-        }
-        return 0;
+                "Idade: " + idade + "\n" +
+                "Vacinado: " + (vacinado ? "Vacinado" : "Não vacinado"+"\n");
     }
 }
